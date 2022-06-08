692a3ebe19a057ab308287ba0aba55f78efcd777
Pentelukin <admin@zuppapeople.com>
2022-06-08 19:55:01 +0300

    Update Angular version to v13

---
+++ dist/ng2-tel-input.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+/// <amd-module name="ng2-tel-input" />
+export * from './public_api';
---
+++ dist/src/ng2-tel-input.module.d.ts
@@ -0,0 +1,9 @@
+import { ModuleWithProviders } from '@angular/core';
+import * as i0 from "@angular/core";
+import * as i1 from "./ng2-tel-input";
+export declare class Ng2TelInputModule {
+    static forRoot(): ModuleWithProviders<Ng2TelInputModule>;
+    static ɵfac: i0.ɵɵFactoryDeclaration<Ng2TelInputModule, never>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<Ng2TelInputModule, [typeof i1.Ng2TelInput], never, [typeof i1.Ng2TelInput]>;
+    static ɵinj: i0.ɵɵInjectorDeclaration<Ng2TelInputModule>;
+}
